5443cf9b0bf414edcb7d426dd3d03d168c81c49d
tj0vtj0v <burdorftjorven@gmail.com>
2023-10-26 09:00:54 +0200

    Tjorven; 23-10-26; Abgabe_7 started

---
+++ Programmieren 1/Abgabe_7/src/gameoflife/Cell.java
@@ -0,0 +1,47 @@
+package gameoflife;
+
+class Cell {
+    private boolean alive;
+    private int livingNeighbors;
+    private final Cell[] neighbors;
+
+    Cell(double probabilityToLive) {
+        this.neighbors = new Cell[8];
+        this.alive = Math.random() < probabilityToLive;
+    }
+
+    void addNeighbor(Cell neighbor) {
+        for (int i = 0; i < this.neighbors.length; i++) {
+            if (neighbors[i] == null) {
+                neighbors[i] = neighbor;
+                break;
+            }
+        }
+    }
+
+    void countLivingNeighbors() {
+        this.livingNeighbors = 0;
+
+        for (Cell cell : this.neighbors) {
+            if (cell != null && cell.alive) {
+                livingNeighbors++;
+            }
+        }
+    }
+
+    void applyRules() {
+        switch (this.livingNeighbors) {
+            case 3:
+                this.alive = true;
+                break;
+            case 2:
+                break;
+            default:
+                this.alive = false;
+        }
+    }
+
+    String asString() {
+        return this.alive ? "X " : "  ";
+    }
+}
