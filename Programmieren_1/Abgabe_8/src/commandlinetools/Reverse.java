6b03dd63ca9cbeec8b6b3f19594bd4cf3587e6c2
Tjorven <burdorftjorven@gmail.com>
2023-11-03 20:56:20 +0100

    Tjorven; 23-11-03; uni 231102

--- Programmieren_1/Abgabe_8/src/commandlinetools/Reverse.java
+++ Programmieren_1/Abgabe_8/src/commandlinetools/Reverse.java
@@ -1,17 +1,13 @@
 package commandlinetools;
 
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Reverse {
-
     public static void main(String[] args) {
         if (args.length != 1 || !args[0].equals("-char-by-char") && !args[0].equals("-line-by-line")) {
             System.out.println("Es wurden unpassende Aufrufargumente übergeben.");
             System.exit(1);
         }
-
 
         String nextLine;
         StringBuilder inputText = new StringBuilder();
@@ -32,13 +28,12 @@ public class Reverse {
 
         StringBuilder outputText = new StringBuilder();
         if (args[0].equals("-char-by-char")) {
-            outputText = inputText;
-            outputText.reverse();
+            outputText = inputText.reverse();
         } else {
-            String bufferText = inputText.toString();
-            String[] linesOfText = bufferText.split("\n");
-            for (String line : linesOfText) {
-                outputText.insert(0, line).append("\n");
+            String[] linesOfText = inputText.toString().split("\\R");
+
+            for (int i = linesOfText.length -1; i >= 0; i--) {
+                outputText.append(linesOfText[i].strip()).append("\n");
             }
         }
 
