46151f9fafcca458f8a9deb66f2db3a57dda4186
tj0vtj0v <burdorftjorven@gmail.com>
2024-04-22 14:53:55 +0200

    algodat update Problem 6.3

--- Algorithmen und Datenstrukturen/Problem 6.3/main.py
+++ Algorithmen und Datenstrukturen/Problem 6.3/main.py
@@ -3,18 +3,19 @@ import math
 from PointMaker import PointMaker
 
 if __name__ == "__main__":
-    for point_maker in (iter(PointMaker(.001)), iter(PointMaker(0, True, 10_000_000))):
-        inside = 0
-        outside = 0
+    point_maker = PointMaker(0, True, 100)
 
-        for point in point_maker:
-            if point.vector_length() > 1:
-                outside += 1
-            else:
-                inside += 1
+    inside = 0
+    outside = 0
 
-        my_pi = 4 * inside / (outside + inside)
+    for point in point_maker:
+        if point.vector_length() > 1:
+            outside += 1
+        else:
+            inside += 1
 
-        print("my pi:  ", my_pi)
-        print("real pi:", math.pi)
-        print("AE:     ", abs(math.pi - my_pi), "\n")
+    my_pi = 4 * inside / (outside + inside)
+
+    print("my pi:  ", my_pi)
+    print("real pi:", math.pi)
+    print("AE:     ", abs(math.pi - my_pi), "\n")
