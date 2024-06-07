ca27dc0efdf6b9c9ccb424a8b98328f98df1a96d
tj0vtj0v <burdorftjorven@gmail.com>
2024-06-07 21:32:20 +0200

    algodat Bonus Problem finished

--- Algorithmen und Datenstrukturen/Problem 12.6/DecisionTreeLeaning.py
+++ Algorithmen und Datenstrukturen/Problem 12.6/DecisionTreeLeaning.py
@@ -1,13 +1,9 @@
 import math
 import random
-from pprint import pprint
 
 """
 examples: [{
-    "dependencies": [{
-        "attribute_name": "sth",
-        "value": "sth"
-        }],
+    "dependencies": {"attribute_name": "value},
     "result": bool
 }]
 
@@ -24,56 +20,37 @@ tree: {
 }
 """
 
-exmpls = [
-    {"dependencies": [{"visible": 0, "distance": "< 10", "armed": 0, }], "result": True},
-    {"dependencies": [{"visible": 0, "distance": "< 10", "armed": 1, }], "result": False},
-    {"dependencies": [{"visible": 0, "distance": "10 - 20", "armed": 0, }], "result": False},
-    {"dependencies": [{"visible": 0, "distance": "10 - 20", "armed": 1, }], "result": False},
-    {"dependencies": [{"visible": 0, "distance": "> 20", "armed": 0, }], "result": False},
-    {"dependencies": [{"visible": 0, "distance": "> 20", "armed": 1, }], "result": False},
-    {"dependencies": [{"visible": 1, "distance": "< 10", "armed": 0, }], "result": True},
-    {"dependencies": [{"visible": 1, "distance": "< 10", "armed": 1, }], "result": True},
-    {"dependencies": [{"visible": 1, "distance": "10 - 20", "armed": 0, }], "result": True},
-    {"dependencies": [{"visible": 1, "distance": "10 - 20", "armed": 1, }], "result": True},
-    {"dependencies": [{"visible": 1, "distance": "> 20", "armed": 0, }], "result": False},
-    {"dependencies": [{"visible": 1, "distance": "> 20", "armed": 1, }], "result": False}
-]
-
-atrbts = [
-    {"name": "visible", "values": [0, 1]},
-    {"name": "distance", "values": ["< 10", "10 - 20", "> 20"]},
-    {"name": "armed", "values": [0, 1]}
-]
-
 
 def DT_LEARNING(examples: list[dict], attributes: list[dict], parent_examples: list[dict]) -> bool | dict:
+    """recursive decision tree learning algorithm as discussed in the lecture"""
     tree = {}
 
     if not examples:
         return PLURALITY_VAL(parent_examples)
-    elif same_classification(examples):
+    elif _same_classification(examples):
         return examples[0]["result"]
     elif not attributes:
         return PLURALITY_VAL(examples)
     else:
-        attribute = _maximise_importance(attributes, examples)
+        attribute = _argmax_importance(attributes, examples)
         tree["attribute"] = attribute["name"]
         attributes.remove(attribute)
     for value in attribute["values"]:
         child_examples = _determine_child_examples(attribute["name"], value, examples)
 
-        tree[value] = DT_LEARNING(child_examples, attributes, examples)
-
+        tree[value] = DT_LEARNING(child_examples, attributes.copy(), examples)
 
     return tree
 
 
 def PLURALITY_VAL(examples: list[dict]) -> bool:
+    """calculates the most common result in the examples"""
     true_examples = _count_true_examples(examples)
     return true_examples > len(examples) / 2
 
 
 def IMPORTANCE(attribute: dict, examples: list[dict]) -> float:
+    """computes the importance of an attribute"""
     D = _count_true_examples(examples) / len(examples)
     information_gain = _binary_entropy(D)
 
@@ -86,6 +63,7 @@ def IMPORTANCE(attribute: dict, examples: list[dict]) -> float:
 
 
 def _count_true_examples(examples: list[dict]) -> int:
+    """counts the examples with the output value true"""
     true_count = 0
     for example in examples:
         if example["result"]:
@@ -95,27 +73,27 @@ def _count_true_examples(examples: list[dict]) -> int:
 
 
 def _binary_entropy(q: float) -> float:
+    """calculates the binary entropy of input q"""
     return 0 if q == 1 or q == 0 else -(q * math.log2(q) + (1 - q) * math.log2(1 - q))
 
 
 def _attribute_occurrence(examples: list[dict], attribute_name: str, attribute_value: any) -> tuple[int, int]:
+    """returns the overall and positive occurrence of an attribute in the given examples"""
     occurrence = 0
     positive_occurrence = 0
 
     for example in examples:
-        for dependency in example["dependencies"]:
-            if dependency[attribute_name] == attribute_value:
+        if example["dependencies"][attribute_name] == attribute_value:
 
-                occurrence += 1
-                if example["result"]:
-                    positive_occurrence += 1
-
-                break
+            occurrence += 1
+            if example["result"]:
+                positive_occurrence += 1
 
     return occurrence, positive_occurrence
 
 
-def same_classification(examples: list[dict]) -> bool:
+def _same_classification(examples: list[dict]) -> bool:
+    """returns if all examples lead to the same result"""
     classification = examples[0]["result"]
     for example in examples:
         if example["result"] != classification:
@@ -124,19 +102,18 @@ def same_classification(examples: list[dict]) -> bool:
 
 
 def _determine_child_examples(attribute_name: dict, attribute_value: str, examples: list[dict]) -> list[dict]:
+    """filters examples for an specific attribute value"""
     child_examples = []
 
     for example in examples:
-        for dependency in example["dependencies"]:
-            if dependency[attribute_name] == attribute_value:
-                child_examples.append(example)
-
-            break
+        if example["dependencies"][attribute_name] == attribute_value:
+            child_examples.append(example)
 
     return child_examples
 
 
-def _maximise_importance(attributes: list[dict], examples: list[dict]) -> dict:
+def _argmax_importance(attributes: list[dict], examples: list[dict]) -> dict:
+    """equivalent of an argmax function for the IMPORTANCE function"""
     maximum_importance = 0
     maximum_attributes = []
 
@@ -151,6 +128,3 @@ def _maximise_importance(attributes: list[dict], examples: list[dict]) -> dict:
             maximum_attributes.append(attribute)
 
     return random.choice(maximum_attributes)
-
-
-pprint(DT_LEARNING(exmpls, atrbts, exmpls))
